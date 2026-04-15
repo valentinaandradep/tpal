5dca00c9210c2a9799d5d409de61d296e841be6d
valentinaandradep <valentinaandradedepedro1@gmail.com>
2026-04-15 19:12:05 -0300

    Add files via upload

---
+++ numeroaleatorio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) 
+{
+
+    if (argc < 3) 
+    {
+        printf("Você precisa informar os limites para geração do número\n");
+        return 0;
+    }
+
+    int min = atoi(argv[1]);
+    int max = atoi(argv[2]);
+
+    srand(time(NULL));
+
+    int resultado = min + rand() % (max - min + 1);
+
+    printf("O número aleatório é %d\n", resultado);
+
+    return 0;
+}
